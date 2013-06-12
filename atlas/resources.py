3a3da49c10bab945cb01a76ca02ee56105fa87e8
Matthew Hummer <mattalui@gmail.com>
2013-06-12 12:05:00 -0600

    removed SearchResource and added SinglePostResource.

--- atlas/resources.py
+++ atlas/resources.py
@@ -1,9 +1,10 @@
 from twisted.web.resource import Resource
 import cgi
-from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING
 import datetime
 from twisted.web.util import redirectTo
 from jinja2 import Environment, FileSystemLoader
+from bson.objectid import ObjectId
 env = Environment(loader=FileSystemLoader('templates'))
 
 
@@ -22,20 +23,24 @@ class CreateResource(Resource):
         blogpost = {'author': cgi.escape(request.args['author'][0]),
                     'text': cgi.escape(request.args['text'][0]),
                     'tags': cgi.escape(request.args['tags'][0]),
-                    'date': datetime.datetime.utcnow()}
+                    'date': datetime.datetime.utcnow(), }
         posts = self.db.posts
         posts.insert(blogpost)
         return redirectTo('read', request)
 
 
-class SearchResource(Resource):
-    def __init__(self):
+class SinglePostResource(Resource):
+    def __init__(self, post_id):
         Resource.__init__(self)
         self.client = MongoClient('localhost', 27017)
         self.db = self.client.atlas
+        self.post_id = post_id
 
     def render_GET(self, request):
-        return env.get_template('search.html').render().encode('utf-8')
+        blogposts = self.db.posts.find({'_id': ObjectId(self.post_id)})
+        template = env.get_template('singlepost.html')
+        context = {'posts': blogposts}
+        return template.render(context).encode('utf-8')
 
     def render_POST(self, request):
         pass
@@ -48,7 +53,14 @@ class ReadResource(Resource):
         self.db = self.client.atlas
 
     def render_GET(self, request):
-        return env.get_template('read.html').render().encode('utf-8')
+        blogposts = self.db.posts.find().sort('date', DESCENDING)
+
+        template = env.get_template('read.html')
+        context = {'posts': blogposts}
+        return template.render(context).encode('utf-8')
+
+    def getChild(self, path, request):
+        return SinglePostResource(path)
 
     def render_POST(self, request):
-        pass
+        return env.get_template('nope.html').render().encode('utf-8')
--- crud.py
+++ crud.py
@@ -2,13 +2,12 @@ from twisted.internet import reactor
 from twisted.web.server import Site
 from twisted.web.resource import Resource
 
-from atlas.resources import CreateResource, SearchResource, ReadResource
+from atlas.resources import CreateResource, ReadResource
 
 
 root = Resource()
 root.putChild("create", CreateResource())
-root.putChild("search", SearchResource())
-root.putChild("read", ReadResource())
+root.putChild("posts", ReadResource())
 factory = Site(root)
 reactor.listenTCP(8080, factory)
 reactor.run()
