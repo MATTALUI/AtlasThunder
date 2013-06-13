a508a63c0eacd0d0abd5c052c9b27da01034d154
Paul Hummer <paul@eventuallyanyway.com>
2013-06-12 22:59:22 -0600

    Add jsonfig and a config for setting the port of the local server.
    Settings for the site can be set inside atlas/config.json, rather than inside
    the code itself.

--- crud.py
+++ crud.py
@@ -1,9 +1,10 @@
 from twisted.internet import reactor
 from twisted.web.server import Site
 
+from atlas.config import config
 from atlas.resources import build_resource
 
 
 factory = Site(build_resource())
-reactor.listenTCP(8080, factory)
+reactor.listenTCP(config['port'], factory)
 reactor.run()
