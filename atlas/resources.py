aa28404c632813e0cd4c15abfc10b5abcba5115a
Matthew Hummer <mattalui@gmail.com>
2013-07-15 10:41:02 -0600

    txmongo and added login

--- atlas/resources.py
+++ atlas/resources.py
@@ -3,26 +3,23 @@ from twisted.web.server import NOT_DONE_YET
 import txmongo
 from txmongo import filter as _filter
 import cgi
-#from txmongo import MongoClient, DESCENDING
-#from pymongo import MongoClient, DESCENDING
 import datetime
-from twisted.web.util import redirectTo
 from txmongo._pymongo.objectid import ObjectId
 from twisted.web.static import File
-from atlas.config import config
 from atlas.template import render_response
-
 _client = txmongo.lazyMongoConnectionPool()
 _db = _client.atlas
+
 
 class PostResource(Resource):
     def __init__(self, post_id):
         Resource.__init__(self)
 
         self.post_id = post_id
+
     def render_GET(self, request):
         def handle_post(post):
-            context = {'post' : post}
+            context = {'post': post}
             request.write(render_response('singlepost.html', context))
             request.finish()
         _id = ObjectId(self.post_id)
@@ -34,6 +31,7 @@ class PostResource(Resource):
 class PostsResource(Resource):
     def __init__(self):
         Resource.__init__(self)
+
     def render_GET(self, request):
         def handle_posts(posts):
             context = {'posts': posts}
@@ -43,22 +41,24 @@ class PostsResource(Resource):
         deferred = _db.posts.find(filter=f)
         deferred.addCallback(handle_posts)
         return NOT_DONE_YET
-        #return render_response('posts.html', context)
+
     def getChild(self, path, request):
         return PostResource(path)
+
 
 class AdminCreateResource(Resource):
     def __init__(self):
         Resource.__init__(self)
-#        self.client = MongoClient('localhost', 27017)
-     #   self.db = self.client.atlas
+
     def render_GET(self, request):
+        if not request.getCookie('username') == 'matt':
+            request.redirect('/login')
+            return ''
         return render_response('create.html')
 
     def render_POST(self, request):
         def finish(posts):
             request.redirect('/admin')
-            
             request.finish()
         blogpost = {'author': cgi.escape(request.args['author'][0]),
                     'text': cgi.escape(request.args['text'][0]),
@@ -69,12 +69,17 @@ class AdminCreateResource(Resource):
         d.addCallback(finish)
         return NOT_DONE_YET
 
+
 class AdminUpdateResource(Resource):
     def __init__(self, post_id=None):
         Resource.__init__(self)
         self.post_id = post_id
 
     def render_GET(self, request):
+        if not request.getCookie('username') == 'matt':
+            request.redirect('/login')
+            return ''
+
         def handle_post(post):
             context = {'post': post}
             request.write(render_response('create.html', context))
@@ -82,25 +87,31 @@ class AdminUpdateResource(Resource):
         d = _db.posts.find_one(ObjectId(self.post_id))
         d.addCallback(handle_post)
         return NOT_DONE_YET
+
     def render_POST(self, request):
         def finish(_):
             request.redirect('/admin')
             request.finish()
         data = {'author': cgi.escape(request.args['author'][0]),
-                    'text': cgi.escape(request.args['text'][0]),
-                    'tags': cgi.escape(request.args['tags'][0]),}
-        d = _db.posts.update({'_id': ObjectId(cgi.escape(request.args['id'][0]))}, {'$set': data})
+                'text': cgi.escape(request.args['text'][0]),
+                'tags': cgi.escape(request.args['tags'][0]), }
+        d = _db.posts.update(
+            {'_id': ObjectId(cgi.escape(request.args['id'][0]))},
+            {'$set': data})
         d.addCallback(finish)
         return NOT_DONE_YET
+
     def getChild(self, path, request):
         return AdminUpdateResource(post_id=path)
+
 
 class AdminDeleteResource(Resource):
     def __init__(self, post_id):
         Resource.__init__(self)
-  #      self.client = MongoClient('localhost', 27017)
-       # self.db = self.client.atlas
         self.post_id = post_id
+#    if not request.getCookie('username') == 'matt':
+#        request.redirect('/login')
+#        return ''
 
     def render_POST(self, request):
         pass
@@ -108,6 +119,10 @@ class AdminDeleteResource(Resource):
 
 class AdminReadResource(Resource):
     def render_GET(self, request):
+        if not request.getCookie('username') == 'matt':
+            request.redirect('/login')
+            return ''
+
         def handle_posts(posts):
             context = {'posts': posts}
             request.write(render_response('read.html', context))
@@ -122,16 +137,44 @@ class AdminReadResource(Resource):
             return AdminUpdateResource()
         elif path == 'new':
             return AdminCreateResource()
-        raise
+        raise Exception('unknown path: {0}'.format(path))
 
     def render_POST(self, request):
         return render_response('nope.html')
 
 
+class LoginResource(Resource):
+    def __init__(self):
+        Resource.__init__(self)
+
+    def render_GET(self, request):
+        request.write(render_response('login.html'))
+        request.finish()
+        return NOT_DONE_YET
+
+    def render_POST(self, request):
+        username = cgi.escape(request.args['username'][0])
+        password = cgi.escape(request.args['password'][0])
+        if username == 'matt' and password == 'cats':
+            request.addCookie('username', 'matt')
+            request.redirect('/admin')
+            return ''
+        else:
+            request.redirect('/login')
+            return ''
+
+
+class LogoutResource(Resource):
+    def render_GET(self, request):
+        request.addCookie('username', None)
+        request.redirect('/posts')
+        return ''
 RESOURCE_MAPPING = {
     'admin': AdminReadResource(),
     'static': File('static'),
-    'posts' : PostsResource(),
+    'posts': PostsResource(),
+    'login': LoginResource(),
+    'logout': LogoutResource(),
 }
 
 
